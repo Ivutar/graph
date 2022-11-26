17d1bd246ca06233d4dbc0578483dac43edd103e
ast <atrusov@gmail.com>
2022-11-26 08:32:31 +0300

    aqua

--- Form1.cs
+++ Form1.cs
@@ -4,50 +4,90 @@ namespace graph;
 
 public partial class Form1 : Form
 {
-    Image fish;
+    Image leftfish;
+    Image rightfish;
+    Image food;
+    Image buble;
     Bitmap buffer;
     Graphics back;
-    List<Swimmer> swimmerList;
+    List<Swimmer> list;
+    //List<Fish> swimmerList;
+    //List<Food> foodList;
 
     public Form1()
     {
         InitializeComponent();
         buffer = new Bitmap(pictureBox1.Width, pictureBox1.Height);
         back = Graphics.FromImage(buffer);
-        fish = Image.FromFile("fish.png");
-        swimmerList = new List<Swimmer>();
-        for (int i = 0; i < 1000; i++)
+        leftfish = Image.FromFile("fish.png");
+        rightfish = Image.FromFile("fish.png");
+        rightfish.RotateFlip(RotateFlipType.RotateNoneFlipX);
+        food = Image.FromFile("worm.png");
+        buble = Image.FromFile("buble.png");
+        list = new List<Swimmer>();
+        for (int i = 0; i < 4; i++)
             AddFish();
+        for (int i = 0; i < 1; i++)
+            AddFood();
+        for (int i = 0; i < 6; i++)
+            AddBuble();
     }
 
     private void AddFish()
     {
-        var f = new Swimmer();
-        f.wd = fish.Width;
-        f.hg = fish.Height;
+        var f = new Fish();
+        f.wd = leftfish.Width;
+        f.hg = leftfish.Height;
         f.x = Random.Shared.Next(0, pictureBox1.Width - f.wd);
         f.y = Random.Shared.Next(0, pictureBox1.Height - f.hg);
         f.dx = Random.Shared.Next(-10, 11);
         f.dy = Random.Shared.Next(-10, 11);
-        swimmerList.Add(f);
+        f.leftfish = leftfish;
+        f.rightfish = rightfish;
+        list.Add(f);
+    }
+
+    private void AddFood()
+    {
+        var f = new Food();
+        f.wd = food.Width;
+        f.hg = food.Height;
+        f.x = Random.Shared.Next(0, pictureBox1.Width - f.wd);
+        f.y = 0;
+        f.food = food;
+        list.Add(f);
+    }
+    private void AddBuble()
+    {
+        var f = new Buble();
+        f.wd = buble.Width;
+        f.hg = buble.Height;
+        f.x = Random.Shared.Next(0, pictureBox1.Width - f.wd);
+        f.y = pictureBox1.Height - f.hg;
+        f.buble = buble;
+        list.Add(f);
     }
 
     private void timer1_Tick(object sender, EventArgs e)
     {
         back.FillRectangle(Brushes.NavajoWhite, 0, 0, pictureBox1.Width, pictureBox1.Height);     
-        foreach(var s in swimmerList)
+        foreach(Swimmer s in list)
         {
-            var nx = s.x + s.dx;
-            var ny = s.y + s.dy;
-            if (nx < 0 || nx > pictureBox1.Width - s.wd) s.dx = -s.dx;
-            if (ny < 0 || ny > pictureBox1.Height - s.hg) s.dy = -s.dy;
-
-            s.x += s.dx;
-            s.y += s.dy;
-            back.DrawImage(fish, s.x, s.y);
+            s.Move(pictureBox1.Width, pictureBox1.Height);
+            s.Draw(back);
         }
 
         using var g = pictureBox1.CreateGraphics();
         g.DrawImage(buffer, 0, 0);
+    }
+
+    private void Form1_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.KeyCode == Keys.Space)
+            AddFish();
+        if (e.KeyCode == Keys.Enter)
+            AddFood();
+        if (e.KeyCode == Keys.Back)
+            AddBuble();
     }
 }
--- Swimmer.cs
+++ Swimmer.cs
@@ -1,11 +1,81 @@
-﻿namespace graph;
+﻿using System.Windows.Forms;
+
+namespace graph;
 
 class Swimmer
 {
     public int x;
     public int y;
-    public int dx;
-    public int dy;
     public int wd;
     public int hg;
+
+    public virtual void Move(int width, int height) { }
+    public virtual void Draw(Graphics g) { }
+}
+
+class Fish : Swimmer
+{
+    public int dx;
+    public int dy;
+    public Image leftfish;
+    public Image rightfish;
+
+    public override void Move(int width, int height)
+    {
+        var nx = x + dx;
+        var ny = y + dy;
+        if (nx < 0 || nx > width - wd) dx = -dx;
+        if (ny < 0 || ny > height - hg) dy = -dy;
+
+        x += dx;
+        y += dy;
+    }
+
+    public override void Draw(Graphics g)
+    {
+        g.DrawImage(dx < 0 ? leftfish : rightfish, x, y);
+    }
+}
+
+class Food : Swimmer
+{
+    public int Satuation;
+    public Image food;
+
+    public override void Move(int width, int height)
+    {
+        const int food_speed = 10;
+
+        var ny = y + food_speed;
+        if (ny < height - hg) y = ny;
+    }
+
+    public override void Draw(Graphics g)
+    {
+        g.DrawImage(food, x, y);
+    }
+}
+
+class Buble : Swimmer
+{
+    public Image buble;
+
+    public override void Move(int width, int height)
+    {
+        const int buble_speed = -5;
+
+        var ny = y + buble_speed;
+        if (ny > 0)
+            y = ny;
+        else
+        {
+            y = height - hg;
+            x = Random.Shared.Next(0, width - wd);
+        }
+    }
+
+    public override void Draw(Graphics g)
+    {
+        g.DrawImage(buble, x, y);
+    }
 }
